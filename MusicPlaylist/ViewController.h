83175aa3404f290704b43b3ac77b4db3c4596c00
Alextnelson <alexander.thomas.nelson@gmail.com>
2015-10-20 14:03:48 -0500

    Initial Commit

---
+++ MusicPlaylist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlaylist
+//
+//  Created by Alexander Nelson on 10/20/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MusicPlaylist/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MusicPlaylist
+//
+//  Created by Alexander Nelson on 10/20/15.
+//  Copyright © 2015 Jetwolfe Labs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
